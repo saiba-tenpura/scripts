9fbf5e8714a4e966724c82260a37de620dc20fa1
Saiba Tenpura <61359816+saiba-tenpura@users.noreply.github.com>
2026-05-05 21:10:21 +0200

    Optimize android to PC sync script, adjust config file to match schema.

--- android-to-pc/android-to-pc.sh
+++ android-to-pc/android-to-pc.sh
@@ -1,26 +1,38 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 shopt -s nullglob
 
 # Mount all available connected MTP devices
 gio mount -li | awk -F= '{if(index($2,"mtp") == 1)system("gio mount "$2)}'
 
 # Source config with base_target & sources
-source $1
+source "$1"
 
 # Copy documents, pictures and videos
-declare -A type_to_extension=([Audio]='*.aac *.wav' [Documents]='*.json *.md *.opus *.pdf *.stl *.txt *.vcf *.zip' [Pictures]='*.bmp *.gif *.jpg *.jpeg *.png *.tgs *.tif *.tiff *.webp' [Videos]='*.mp4 *.webm')
-for source in "${sources[@]}"; do
-    printf 'Dir: %s \n' "${source}"
-    cd "$source"
+declare -A type_to_extension=(
+  [Audio]='*.aac *.wav'
+  [Documents]='*.json *.md *.opus *.pdf *.stl *.txt *.vcf *.zip'
+  [Pictures]='*.bmp *.gif *.jpg *.jpeg *.png *.tgs *.tif *.tiff *.webp'
+  [Videos]='*.mp4 *.webm'
+)
+
+for src in "${sources[@]}"; do
+    printf 'Dir: %s \n' "$src"
     for type in "${!type_to_extension[@]}"; do 
-        printf 'Type: %s \n' "${type}"
-        for file in ${type_to_extension[$type]}; do
-            mod_date="$(date +%F -r "${file}")"
-            year="${mod_date:0:4}"
-            mkdir -p "$base_target/$year/$type"
-            printf 'File: %s to %s \n' "${file}" "$base_target/$year/$type" 
-            rsync -avz "$file" "$base_target/$year/$type" > /dev/null
+        printf 'Type: %s \n' "$type"
+        for extension in ${type_to_extension[$type]}; do
+            for file in "$src"/$pattern; do
+                [[ -e "$file" ]] || continue
+
+                year="$(date -r "$file" +%Y)"
+                target_dir="$base_target/$year/$type"
+
+                mkdir -p "$target_dir"
+                printf 'File: %s to %s \n' "$file" "$target_dir"
+                rsync -avz "$file" "$target_dir/" > /dev/null
+            done
         done
 
         printf '\n'
